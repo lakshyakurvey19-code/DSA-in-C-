63b681c163de084eb7a847ff2a45dea69842a69b
lakshyakurvey19-code <lakshyakurvey19@gmail.com>
2026-09-11 00:14:02 +0530

    Add Pascal's triangle and pyramid pattern programs

--- 5._function/012_pascal_pyramid.cpp
+++ 5._function/012_pascal_pyramid.cpp
@@ -19,6 +19,9 @@ int main(){
     cin >> n;
 
     for(int i=0; i<=n; i++){
+        for(int j=0; j<=n-i-1; j++){
+            cout <<" ";
+        }
         for(int j=0; j<=i; j++){
             cout << ncr(i,j)<<" ";
         }
